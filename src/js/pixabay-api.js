f10f4a66a4585f8e09833a3147b3028fb84573ec
vladislav-chihun <vladikkosmo@gmail.com>
2024-04-05 20:25:28 +0200

    f

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,12 @@
 
-function getUser(key,q,image_type,orientation,safesearch) {
+function getPhotos(key,q,image_type,orientation,safesearch) {
     const API_KEY = '43242256-64b8ba54a0ed56e09a2e1fe41';
-    const BASE_URL = "https://pixabay.com/api/?" + API_KEY + "&q=" + encodeURIComponent('red roses');
+    const BASE_URL = "https://pixabay.com/api/?" + API_KEY;
     const params = new URLSearchParams({
         key: `${API_KEY}`,
-        q:"",
-        image_type: photo,
-        orientation: horizontal,
+        q:"yellolw",
+        image_type: "photo",
+        orientation: "horizontal",
         safesearch: true,
     })
     const URL = `${BASE_URL}${params}`;
@@ -17,5 +17,6 @@ function getUser(key,q,image_type,orientation,safesearch) {
             "X-RateLimit-Reset": "",
         }
     }
-    fetch(url, options);
+    return fetch(URL, options).then(res => res.json());
 }
+getPhotos().then(data => { console.log(data) })

8f68881195a2133c76aeb4bba5174e3dce20a48b
vladislav-chihun <vladikkosmo@gmail.com>
2024-04-05 19:56:00 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,21 @@
-function getTest() {
-    const BASE_URL = "";
-    const ENDPOINT = "";
-    const URL = `${BASE_URL}${ENDPOINT}`
-    return fetch(url).then(res => res.json());
+
+function getUser(key,q,image_type,orientation,safesearch) {
+    const API_KEY = '43242256-64b8ba54a0ed56e09a2e1fe41';
+    const BASE_URL = "https://pixabay.com/api/?" + API_KEY + "&q=" + encodeURIComponent('red roses');
+    const params = new URLSearchParams({
+        key: `${API_KEY}`,
+        q:"",
+        image_type: photo,
+        orientation: horizontal,
+        safesearch: true,
+    })
+    const URL = `${BASE_URL}${params}`;
+    const options = {
+        headers: {
+            "X-RateLimit-Limit": "",
+            "X-RateLimit-Remaining": "",
+            "X-RateLimit-Reset": "",
+        }
+    }
+    fetch(url, options);
 }

efabf08f1ab9ee3261f348db44da1ec41c85a385
vladislav-chihun <vladikkosmo@gmail.com>
2024-04-06 09:36:37 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,22 +1,14 @@
-
-function getPhotos(key,q,image_type,orientation,safesearch) {
+function getPhotos(q) {
     const API_KEY = '43242256-64b8ba54a0ed56e09a2e1fe41';
     const BASE_URL = "https://pixabay.com/api/?key=" + API_KEY;
     const params = new URLSearchParams({
         key: `${API_KEY}`,
-        q:"yellolw",
+        q: `${q}`,
         image_type: "photo",
         orientation: "horizontal",
         safesearch: true,
     })
-    const URL = `${BASE_URL}${params}`;
-    const options = {
-        headers: {
-            "X-RateLimit-Limit": "",
-            "X-RateLimit-Remaining": "",
-            "X-RateLimit-Reset": "",
-        }
-    }
-    return fetch(URL, options).then(res => res.json());
+    const URL = `${BASE_URL}&${params}`;
+    return fetch(URL).then(res => res.json());
 }
-getPhotos().then(data => { console.log(data) })
+getPhotos().then(data => { console.log(data) });

2fad12527237f59ebe23209cd2223e8f09a508e4
vladislav-chihun <vladikkosmo@gmail.com>
2024-04-06 20:02:38 +0200

    f

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,8 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { imageRender } from "./render-functions";
 
 const form = document.querySelector(".form");
